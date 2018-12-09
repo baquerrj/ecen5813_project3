dcf31d8fc7237285216b0d276692c467a4789299
Roberto Baquerizo <roba8460@colorado.edu>
2018-12-09 16:35:45 -0500

    header with macros to compile polling or interrupt implementations

---
+++ source/settings.h
@@ -0,0 +1,14 @@
+/*
+ * settings.h
+ *
+ *  Created on: Dec 9, 2018
+ *      Author: Roberto Baquerizo
+ */
+
+#ifndef SETTINGS_H_
+#define SETTINGS_H_
+
+#define _POLLING_				0
+#define _NON_BLOCKING_	1
+
+#endif /* SETTINGS_H_ */
