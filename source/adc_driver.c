dfbad79a4bcaf818d755bbbf3a318819a34caf8e
Roberto Baquerizo <roba8460@colorado.edu>
2018-12-12 18:03:52 -0700

    Re-implemented ADC init function to use macros

--- source/adc_driver.c
+++ source/adc_driver.c
@@ -1,26 +1,40 @@
 /*
  * adc_driver.c
  *
+ *
  *  Created on: Dec 10, 2018
- *      Author: Roberto Baquerizo
+ *      Author: Roberto Baquerizo, Vance Farren
  */
 #include "adc_driver.h"
 
+/* Initialize ADC0  in differential mode and
+ * with DMA Connection
+ *
+ * @param[in]:		void
+ * @returns:			void
+ */
 void adc_init( void )
 {
-	//suggested flow on p. 501: update CFG1, SC2, SC1n, check in loop if COCO != 1 loopback, else read RN to clear COCO, continue
-	SIM->SCGC6 |= 0x8000000; /* attach clock to ADC0, p. 207 in ref manual */ //select clock input src and divide ratio to generate ADCK
-	ADC0->SC2 &= ~0x40; /* software trigger p. 470*///select conversion trigger, HW or SW, compare fxns
-	ADC0->SC3 =0x0C; //continous/oneshot sampling, HW averaging
-	ADC0->SC1[0] = 0x20; //enable differential mode p.464// select single or differential, enable/disable COCO interrupts, select input channel to perform conversions
-						//differential inputs: p. 459
-						//adc data result p. 468
-						//conversion time calculation 489
-	//ADC0->SC1 = 0x60; //assign adc0 to differential mode use DADP0 pair p . 464
-	/* clock div by 4, long sample time, differential 16 bit, bus clock */
-	ADC0->CFG1 = 0x40 | 0x10 | 0x04 | 0x00; //p. 465 //third param 0x06 for diff input
+	// Enable clocks
+	SIM_SCGC6 |= SIM_SCGC6_ADC0_MASK;	// Attach clock to ADC0
+
+	// Configure ADC
+	ADC0_CFG1 = 0;	// Clear Configuration Register 1
+	ADC0_CFG1 |= (ADC_CFG1_MODE(3)			| /* 16-bit mode */
+								ADC_CFG1_ADICLK(0)		| /* Bus Clock */
+								ADC_CFG1_ADIV(2)			|	/* Clock divide by 4*/
+								ADC_CFG1_ADLSMP_MASK);	/* Long Sample Time */
+
+	ADC0_SC1A	|= ADC_SC1_DIFF_MASK;	/* Enable Differential Mode */
+
+	ADC0_SC3 |= (ADC_SC3_AVGE_MASK	|	/* Enable HW averaging */
+							 ADC_SC3_ADCO_MASK);	/* Continuous Sampling */
+
+
+	ADC0_SC2 &= ADC_SC2_ADTRG_MASK;
 
 	/* DMA Enable */
-	ADC0->SC2 |= ADC_SC2_DMAEN_MASK;
+	ADC0_SC2 |= ADC_SC2_DMAEN_MASK;
+
 	return;
 }
--- source/adc_driver.h
+++ source/adc_driver.h
@@ -2,7 +2,7 @@
  * adc_driver.h
  *
  *  Created on: Dec 10, 2018
- *      Author: Roberto Baquerizo
+ *      Author: Roberto Baquerizo, Vance Farren
  */
 
 #ifndef ADC_DRIVER_H_
@@ -10,6 +10,14 @@
 
 #include "memory_map.h"
 
+/* Initialize ADC0  in differential mode and
+ * with DMA Connection
+ *
+ * @param[in]:		void
+ * @returns:			void
+ */
 void adc_init( void );
 
+
 #endif /* ADC_DRIVER_H_ */
+
